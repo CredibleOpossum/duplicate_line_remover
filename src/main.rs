beafc2fe211d3573a9d7dd2f22f4f3d47e949069
Zander <60998830+CredibleOpossum@users.noreply.github.com>
2021-05-30 21:43:02 -0400

    use stdin

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
 use std::collections::HashSet;
-use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
-use std::process;
+use std::io::{self, BufRead};
 
 fn main() {
-    for file in env::args().skip(1) {
-        match line_remover(file) {
-            Ok(()) => (),
-            Err(e) => {
-                eprintln!("{}", e);
-                process::exit(1);
-            }
-        }
-    }
-}
-
-fn line_remover(file: String) -> Result<(), Box<dyn Error>> {
-    let reader = BufReader::new(File::open(file)?);
+    let stdin = io::stdin();
     let mut seen_lines: HashSet<String> = HashSet::new();
-    for line in reader.lines() {
-        let text = line?;
+    for line in stdin.lock().lines() {
+        let text = line.unwrap();
         if text != "" && !seen_lines.contains(&text) {
             println!("{}", text);
             seen_lines.insert(text);
         }
     }
-    Ok(())
 }
